17098fb00e2b201d1072e2388295ce318acd1c3a
unknown <vasanthgamer2000@gmail.com>
2023-07-13 14:49:55 +0530

    Login Form

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ValiduserName = "vasan@123";
+            string Validpassword = "vth@456";
+            
+
+            int attempts = 3;
+            while(attempts > 0)
+            {
+                Console.WriteLine("Enter Username:\t");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Enter Password");
+                string password = Console.ReadLine();
+
+                if (userName == ValiduserName && password == Validpassword) 
+                {
+                    Console.WriteLine("Login Successfull!");
+                }
+
+                else if(userName == ValiduserName) {
+                    Console.WriteLine("Enter the valid Password");
+                    attempts--;
+                    Console.WriteLine($"You have {attempts} remaining attempts");
+                }
+                else
+                {
+                    Console.WriteLine("Enter the Valid Username");
+                    attempts--;
+                    Console.WriteLine($"You have {attempts} remaining attempts");
+
+                }
+                
+                if (attempts == 0)
+                {
+                    Console.WriteLine("Login Failed");
+
+                }
+               
+
+            }
+            Console.ReadKey();
+
+            
+        }
+    }
+}
